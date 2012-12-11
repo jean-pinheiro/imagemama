49c8ba61425833dd99fb2cd788218770a01d5080
tao-tiago <tao.tiago@hotmail.com>
2012-12-11 19:13:35 -0300

    index

---
+++ site/wp-content/themes/imagemama/home.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.css">
+
+<div id="body">
+	<div id="header">
+		<div id="top">
+			<a href="<?php bloginfo('template_url') ?>" title="Imagem Mama">
+				<h1 id="logo">Imagem Mama</h1>
+			</a>
+		</div>
+		<div id="menu">teste</div>
+	</div>
+</div>
+<div id="rodape-menu-bottom"></div>
+<div id="rodape-endereco"></div>
