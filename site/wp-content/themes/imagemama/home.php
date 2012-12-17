e53e5d2d212fa86596605a9436fb116496957089
tao-tiago <tao.tiago@hotmail.com>
2012-12-17 15:58:48 -0300

    Mapa Ajax e projeto A,A

--- site/wp-content/themes/imagemama/home.php
+++ site/wp-content/themes/imagemama/home.php
@@ -26,8 +26,8 @@
 					<div id="where-is-here">
 						<div class="where-is-here">Onde Estamos</div>
 					</div>
-						<iframe width="293" height="165" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=+&amp;q=r+coronel+linhares,+950+aldeota&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Cel.+Linhares,+950+-+Aldeota,+Fortaleza+-+Cear%C3%A1,+60170-241&amp;ll=-3.742006,-38.495895&amp;spn=0.002907,0.005252&amp;t=m&amp;z=14&amp;output=embed">
-						</iframe>
+					<div id="map">
+					</div>
 					<div id="address-1">
 						<p class="first">Medical Ceniter</p>
 						<p class="second">
